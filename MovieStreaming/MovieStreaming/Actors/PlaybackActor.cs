5e9953b5c589b9ff094be5ec275f658144908f79
Mert Akcakaya <mert@meltabi.com>
2016-04-09 15:08:23 +0300

    Creating an actor
    Actor system and an example actor class is implemented.

---
+++ MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreaming.Actors
+{
+    public sealed class PlaybackActor : UntypedActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor");
+        }
+
+        protected override void OnReceive(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,25 @@
+﻿using Akka.Actor;
+using MovieStreaming.Actors;
+using System;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+
+            IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
